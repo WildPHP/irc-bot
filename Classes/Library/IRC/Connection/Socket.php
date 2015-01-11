cb1ab92f89d35d1f958955a7f29d8a77f69c40ad
Yoshi2889 <rick.2889@gmail.com>
2015-01-11 21:28:06 +0100

    And remove my vardump.
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- Classes/Library/IRC/Connection/Socket.php
+++ Classes/Library/IRC/Connection/Socket.php
@@ -92,8 +92,7 @@
           
             if (!empty($pass))
                 $this->sendData('PASS ' . $pass);
-    
-            echo var_dump($nick, $name);
+
             $this->sendData( 'USER ' . $nick . ' Layne-Obserdia.de ' . $nick . ' :' . $name);
             $this->sendData( 'NICK ' . $nick );
         }
