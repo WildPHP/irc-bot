8ac1e73f704bc2fed8439bf4496e1f6a59e264a8
Yoshi2889 <rick.2889@gmail.com>
2015-01-12 17:08:58 +0100

    Fix the Joins listener for servers which put a colon in front of channel names.
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- Classes/Listener/Joins.php
+++ Classes/Listener/Joins.php
@@ -9,16 +9,19 @@ namespace Listener;
  * @author Matej Velikonja <matej@velikonja.si>
  */
 class Joins extends \Library\IRC\Listener\Base {
-
 	/**
 	 * Main function to execute when listen even occurs
 	 */
 	public function execute($data) {
 		$args = $this->getArguments($data);
 		$user = \Library\FunctionCollection::getUserNickName($args[0]);
+		
+		// Remove any leading colons, if they exist.
+		if (substr($args[2], 0, 1) == ':')
+			$args[2] = substr($args[2], 1);
 
-		if ($user != 'FatalException')
-		$this->say('Welcome ' . $user . '!', $args[2]);
+		if ($user != $this->bot->getNick())
+			$this->say('Welcome ' . $user . '!', $args[2]);
 	}
 
 	/**
