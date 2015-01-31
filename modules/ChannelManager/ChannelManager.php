df3a62db9c0c15247acb473c2bf0935857a83bac
Yoshi2889 <rick.2889@gmail.com>
2015-01-31 20:15:10 +0100

    Add a ChannelManager module; implements join and part commands
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ modules/ChannelManager/ChannelManager.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+	WildPHP - a modular and easily extendable IRC bot written in PHP
+	Copyright (C) 2015 WildPHP
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace WildPHP\Modules;
+
+class ChannelManager
+{
+	private $bot;
+	private $channels;
+	public function __construct($bot)
+	{
+		$this->bot = $bot;
+
+		// Register our command.
+		$this->bot->registerEvent(array('command_join', 'command_part'), array('hook_once' => true));
+		$this->bot->hookEvent('command_join', array($this, 'JoinCommand'));
+		$this->bot->hookEvent('command_part', array($this, 'PartCommand'));
+	}
+
+	public function JoinCommand($data)
+	{
+		if (empty($data['string']))
+			return;
+
+		// Join all specified channels.
+		$c = explode(' ', $data['string']);
+
+		foreach ($c as $chan)
+		{
+			$this->bot->log('Joining channel ' . $chan . '...', 'CHANMAN');
+			$this->channels[] = $chan;
+			$this->bot->sendData('JOIN ' . $chan);
+		}
+	}
+
+	public function PartCommand($data)
+	{
+		// Part the current channel.
+		if (empty($data['string']))
+		{
+			$chan = $data['argument'];
+
+			$this->bot->log('Parting channel ' . $chan . '...', 'CHANMAN');
+			$this->bot->sendData('PART ' . $chan);
+
+			return;
+		}
+
+		// Part all specified channels.
+		$c = explode(' ', $data['string']);
+
+		foreach ($c as $chan)
+		{
+			$this->bot->log('Parting channel ' . $chan . '...', 'CHANMAN');
+			$this->bot->sendData('PART ' . $chan);
+		}
+	}
+}
