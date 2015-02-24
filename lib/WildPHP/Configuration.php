5448131fba40c146832a478fb70c3b6beb7604a5
Southparkfan <Southparkfan@users.noreply.github.com>
2015-02-24 22:37:22 +0100

    Fix possible scrutinizer bug

--- lib/WildPHP/Configuration.php
+++ lib/WildPHP/Configuration.php
@@ -49,7 +49,7 @@ class Configuration
 			else
 				throw new \Exception('The configuration could not be loaded. Please check the file ' . $config . ' exists and is readable/not corrupt.');
 		}
-		catch(Exception $e)
+		catch(\Nette\Neon\Exception $e)
 		{
 			throw new \Exception('Configuration syntax error: ' . $e->getMessage() . PHP_EOL);
 		}
