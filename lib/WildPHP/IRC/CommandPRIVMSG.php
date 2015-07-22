88a8c4b5adf8813918f51693764cfe0c0048065e
Yoshi2889 <rick.2889@gmail.com>
2015-07-22 23:09:11 +0200

    Clean up CommandPRIVMSG
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- lib/WildPHP/IRC/CommandPRIVMSG.php
+++ lib/WildPHP/IRC/CommandPRIVMSG.php
@@ -21,7 +21,7 @@ namespace WildPHP\IRC;
 
 use Phergie\Irc\Parser as PhergieParser;
 
-class CommandPRIVMSG implements ICommandPRIVMSG
+class CommandPRIVMSG extends ServerMessage implements ICommandPRIVMSG
 {
 	protected $message;
 	protected $prefix;
@@ -30,34 +30,14 @@ class CommandPRIVMSG implements ICommandPRIVMSG
 	{
 		if (!($ircMessage instanceof ServerMessage))
 			throw new InvalidArgumentException('The provided argument is not an instance of ServerMessage.');
-		
+
 		if ($ircMessage->getCommand() != 'PRIVMSG')
 			throw new InvalidArgumentException('The provided message is not a PRIVMSG command.');
-		
+
 		$this->message = $ircMessage;
 		$this->prefix = (string) $commandPrefix;
 	}
 
-	public function getMessage()
-	{
-		return $this->message->getMessage();
-	}
-
-	public function getCommand()
-	{
-		return $this->message->getCommand();
-	}
-
-	public function getParams()
-	{
-		return $this->message->getParams();
-	}
-
-	public function getPrefix()
-	{
-		return $this->message->getPrefix();
-	}
-	
 	public function getHostname()
 	{
 		return $this->message->get()['user'];
@@ -77,32 +57,27 @@ class CommandPRIVMSG implements ICommandPRIVMSG
 	{
 		return (string) $this->message->getParams()['text'];
 	}
-	
+
 	public function getNickname()
 	{
 		return $this->message->get()['nick'];
-	}
-	
-	public function get()
-	{
-		return $this->message;
 	}
 
 	public function getBotCommand()
 	{
 		$pieces = explode(' ', $this->getUserMessage());
-		
+
 		if (substr($pieces[0], 0, strlen($this->prefix)) != $this->prefix)
 			return false;
-		
+
 		return substr($pieces[0], strlen($this->prefix));
 	}
-	
+
 	public function getBotCommandParams()
 	{
 		if ($this->getBotCommand() === false)
 			return false;
-		
+
 		$pieces = explode(' ', $this->getUserMessage());
 		array_shift($pieces);
 		return $pieces;
