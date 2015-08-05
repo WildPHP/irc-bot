518d26262ad269e8263efc3330f6b885405874c4
Yoshi2889 <rick.2889@gmail.com>
2015-08-05 14:49:17 +0200

    Properly indent ChannelMessageEvent.php
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- modules/ChannelManager/Event/ChannelMessageEvent.php
+++ modules/ChannelManager/Event/ChannelMessageEvent.php
@@ -23,36 +23,36 @@ use WildPHP\IRC\CommandPRIVMSG;
 
 class ChannelMessageEvent implements IChannelEvent
 {
-    /**
-     * The channel.
-     * @var string
-     */
-    protected $channel;
+	/**
+	 * The channel.
+	 * @var string
+	 */
+	protected $channel;
 
-    /**
-     * The message.
-     * @var CommandPRIVMSG
-     */
-    protected $message;
+	/**
+	 * The message.
+	 * @var CommandPRIVMSG
+	 */
+	protected $message;
 
-    /**
-     * Constructs the event.
-     * @param string $channel The channel that is being joined.
-     * @param CommandPRIVMSG $message The message that is received.
-     */
-    public function __construct($channel, CommandPRIVMSG $message)
-    {
-        $this->channel = $channel;
-        $this->message = $message;
-    }
+	/**
+	 * Constructs the event.
+	 * @param string $channel The channel that is being joined.
+	 * @param CommandPRIVMSG $message The message that is received.
+	 */
+	public function __construct($channel, CommandPRIVMSG $message)
+	{
+		$this->channel = $channel;
+		$this->message = $message;
+	}
 
-    public function getChannel()
-    {
-        return $this->channel;
-    }
+	public function getChannel()
+	{
+		return $this->channel;
+	}
 
-    public function getMessage()
-    {
-        return $this->message;
-    }
+	public function getMessage()
+	{
+		return $this->message;
+	}
 }
