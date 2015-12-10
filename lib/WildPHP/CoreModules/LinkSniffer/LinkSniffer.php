c27757e8327c42b6108e366ff4af28cdc290d42d
Yoshi2889 <rick.2889@gmail.com>
2015-12-10 16:53:52 +0100

    LinkSniffer: Internal rewrite
    Cleaned up code, use Remote/ShortenUri APIs
    
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- lib/WildPHP/CoreModules/LinkSniffer/LinkSniffer.php
+++ lib/WildPHP/CoreModules/LinkSniffer/LinkSniffer.php
@@ -1,111 +1,118 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rkerkhof
- * Date: 5-12-15
- * Time: 0:12
- */
+
+/*
+WildPHP - a modular and easily extendable IRC bot written in PHP
+Copyright (C) 2015 WildPHP
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 namespace WildPHP\CoreModules\LinkSniffer;
 
-use GuzzleHttp\Exception\RequestException;
-use Psr\Http\Message\ResponseInterface;
+use WildPHP\API\Remote;
+use WildPHP\API\ShortenUri;
 use WildPHP\BaseModule;
 use WildPHP\CoreModules\Connection\IrcDataObject;
+use WildPHP\Exceptions\ShortUriCreationFailedException;
 
 class LinkSniffer extends BaseModule
 {
+
 	public function setup()
 	{
-		$this->getEventEmitter()->on('irc.data.in.privmsg', array($this, 'sniffLinks'));
+		$this->getEventEmitter()->on('irc.data.in.privmsg', [$this, 'sniffLinks']);
 	}
 
+	/**
+	 * @param IrcDataObject $message
+	 * @return void
+	 */
 	public function sniffLinks(IrcDataObject $message)
 	{
 		$string = $message->getParams()['text'];
 		$target = $message->getTargets()[0];
 
-		$result = preg_match('/(https?:\/\/\S+)/i', $string, $matches);
+		// Break the message up in pieces so we can analyse each.
+		$pieces = explode(' ', $string);
 
-		if (!$result)
+		foreach ($pieces as $piece)
+		{
+			if (Remote::isValidLink($piece))
+				$link = $piece;
+		}
+
+		if (empty($link))
 			return;
 
-		$link = $matches[1];
+		try
+		{
+			$headerResource = Remote::getUriHeaders($link);
 
-		if (!$this->checkValidLink($link))
-			return;
+			if (!$headerResource->hasHeader('Content-Type'))
+				return;
 
-		try {
-			$shorturl = $this->createShortLink($link);
+			$content_type = strtolower(explode(';', $headerResource->getHeaderLine('Content-Type'))[0]);
 
-			if (!$this->checkValidLink($link))
-				return false;
-
-			$httpClient = new \GuzzleHttp\Client();
-
-			$resource = $httpClient->head($link);
-
-			if (!$resource->hasHeader('Content-Type'))
-				return false;
-
-			$content_type = strtolower(explode(';', $resource->getHeaderLine('Content-Type'))[0]);
-
-			if (!in_array($content_type, ['text/html']))
-				$title = '(not a web page, content type: ' . $content_type . ')';
-
-			else
+			$title = '(not a web page, content type: ' . $content_type . ')';
+			if (in_array($content_type, ['text/html']))
 			{
-				$resource = $httpClient->get($link);
-				$body = $resource->getBody();
+				$temp = $this->getTitleFromUri($link);
 
-				$contents = '';
-				$title = '(no title)';
-
-				$maxBytes = 1024 * 1024 * 3; // 3 MB max per page
-				$readBytes = 0;
-				while (!$body->eof() && $readBytes < $maxBytes)
-				{
-					$buffer = $body->read(1024);
-					$readBytes += 1024;
-					$contents .= $buffer;
-
-					$result = preg_match('/\<title\>(.*)\<\/title\>/i', $contents, $matches);
-					if ($result == false)
-						continue;
-
-					if (!empty($matches[1]))
-						$title = htmlspecialchars_decode($matches[1], ENT_QUOTES);
-				}
-				$body->close();
+				if (!empty($temp))
+					$title = $temp;
 			}
 
-			if (empty($shorturl))
-				$shorturl = 'No short url';
+			try
+			{
+				$shortUri = ShortenUri::createShortLink($link);
+			}
+			catch (ShortUriCreationFailedException $e)
+			{
+				$shortUri = 'No short url';
+			}
 
 			$connection = $this->getModule('Connection');
-			$connection->write($connection->getGenerator()->ircPrivmsg($target, '[' . $shorturl . '] ' . $title));
+			$connection->write($connection->getGenerator()
+				->ircPrivmsg($target, '[' . $shortUri . '] ' . $title));
 		}
 		catch (\Exception $e)
-		{}
+		{
+		}
 	}
 
-	public function checkValidLink($link)
+	/**
+	 * @param string $link
+	 * @return string
+	 */
+	public function getTitleFromUri($link)
 	{
-		return filter_var($link, FILTER_VALIDATE_URL) == $link;
-	}
+		$contents = '';
+		$title = '';
+		Remote::getUriBodySplit($link, function ($partial) use (&$title, &$contents)
+		{
+			$contents .= $partial;
 
-	public function createShortLink($link)
-	{
-		if (!$this->checkValidLink($link))
-			return false;
+			if (preg_match('/\<title\>(.*)\<\/title\>/i', $contents, $matches) && !empty($matches[1]))
+			{
+				$title = htmlspecialchars_decode($matches[1], ENT_QUOTES);
 
-		$contents = file_get_contents('http://is.gd/create.php?format=json&url=' . urlencode($link));
+				return false;
+			}
 
-		$result = json_decode($contents);
+			return true;
+		});
 
-		if ($result == false || empty($result->shorturl))
-			return false;
-
-		return $result->shorturl;
+		return $title;
 	}
 }
