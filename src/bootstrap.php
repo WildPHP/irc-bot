89607c3e3828c25312a77f14b3750eb12c98f28a
Yoshi2889 <rick.2889@gmail.com>
2016-05-22 02:44:39 +0200

    TODO: Some more initialisation
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,9 @@ use React\EventLoop\Factory as LoopFactory;
 use WildPHP\Core\Configuration\Configuration;
 use WildPHP\Core\Events\EventEmitter;
 use WildPHP\Core\Logger\Logger;
+use WildPHP\Core\Connection\ConnectionDetailsHolder;
+use WildPHP\Core\Connection\IrcConnection;
+use WildPHP\Core\Connection\Queue;
 
 /*
 	WildPHP - a modular and easily extendable IRC bot written in PHP
@@ -27,8 +30,27 @@ Logger::initialize();
 Configuration::initialize();
 EventEmitter::initialize();
 
-$queue = new \WildPHP\Core\Connection\Queue();
-
 $loop = LoopFactory::create();
-$loop->addPeriodicTimer(1, [$queue, 'flush']);
+$connectorFactory = new \WildPHP\Core\Connection\ConnectorFactory($loop);
+
+ConnectionDetailsHolder::setServer(Configuration::get('server')->getValue());
+ConnectionDetailsHolder::setPort(Configuration::get('port')->getValue());
+ConnectionDetailsHolder::setInitialNickname(Configuration::get('nick')->getValue());
+ConnectionDetailsHolder::setUsername(Configuration::get('user')->getValue());
+ConnectionDetailsHolder::setRealname(Configuration::get('realname')->getValue());
+ConnectionDetailsHolder::setIsSecure(Configuration::get('secure')->getValue());
+
+if (ConnectionDetailsHolder::getIsSecure())
+    $connector = $connectorFactory->createSecure();
+else
+    $connector = $connectorFactory->create();
+
+$ircConnection = new IrcConnection();
+$queue = new Queue();
+$ircConnection->registerQueueFlusher($loop, $queue);
+$ircConnection->createFromConnector($connector);
+
+$queue->user(ConnectionDetailsHolder::getUsername(), gethostname(), ConnectionDetailsHolder::getServer(), ConnectionDetailsHolder::getRealname());
+$queue->nick(ConnectionDetailsHolder::getInitialNickname());
+
 $loop->run();
