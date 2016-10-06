7a51e5e385616cfc173b8512d5f3961f45a72769
Yoshi2889 <rick.2889@gmail.com>
2016-10-06 11:53:23 +0200

    Add implements to JOIN and PRIVMSG incoming messages
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/Connection/IncomingIrcMessages/JOIN.php
+++ src/Connection/IncomingIrcMessages/JOIN.php
@@ -28,7 +28,7 @@ use WildPHP\Core\Connection\UserPrefix;
 use WildPHP\Core\Users\GlobalUserCollection;
 use WildPHP\Core\Users\User;
 
-class JOIN
+class JOIN implements BaseMessage
 {
 	/**
 	 * @var UserPrefix
