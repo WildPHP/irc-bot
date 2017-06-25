dd4d4cab246d4cbcf6079e6f5d982ab32e46db3a
Yoshi2889 <rick.2889@gmail.com>
2017-06-25 14:09:10 +0200

    Add BaseIRCMessage class
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Connection/IRCMessages/BaseIRCMessage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace WildPHP\Core\Connection\IRCMessages;
+
+
+abstract class BaseIRCMessage
+{
+	/**
+	 * @var string
+	 */
+	protected static $verb;
+
+	/**
+	 * @return string
+	 */
+	public static function getVerb(): string
+	{
+		return self::$verb;
+	}
+}
