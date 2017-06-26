0db3f7b6a34965556d8561c15315fdf8762a6eac
Yoshi2889 <rick.2889@gmail.com>
2017-06-26 21:06:03 +0200

    Add ConnectionDetails class, oops
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Connection/ConnectionDetails.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace WildPHP\Core\Connection;
+
+
+class ConnectionDetails
+{
+	/**
+	 * @var string
+	 */
+	protected $username;
+
+	/**
+	 * @var string
+	 */
+	protected $hostname;
+
+	/**
+	 * @var string
+	 */
+	protected $address;
+
+	/**
+	 * @var int
+	 */
+	protected $port;
+
+	/**
+	 * @var string
+	 */
+	protected $realname;
+
+	/**
+	 * @var string
+	 */
+	protected $password;
+
+	/**
+	 * @var string
+	 */
+	protected $wantedNickname;
+
+	/**
+	 * @return string
+	 */
+	public function getUsername(): string
+	{
+		return $this->username;
+	}
+
+	/**
+	 * @param string $username
+	 */
+	public function setUsername(string $username)
+	{
+		$this->username = $username;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHostname(): string
+	{
+		return $this->hostname;
+	}
+
+	/**
+	 * @param string $hostname
+	 */
+	public function setHostname(string $hostname)
+	{
+		$this->hostname = $hostname;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAddress(): string
+	{
+		return $this->address;
+	}
+
+	/**
+	 * @param string $address
+	 */
+	public function setAddress(string $address)
+	{
+		$this->address = $address;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPort(): int
+	{
+		return $this->port;
+	}
+
+	/**
+	 * @param int $port
+	 */
+	public function setPort(int $port)
+	{
+		$this->port = $port;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRealname(): string
+	{
+		return $this->realname;
+	}
+
+	/**
+	 * @param string $realname
+	 */
+	public function setRealname(string $realname)
+	{
+		$this->realname = $realname;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPassword(): string
+	{
+		return $this->password;
+	}
+
+	/**
+	 * @param string $password
+	 */
+	public function setPassword(string $password)
+	{
+		$this->password = $password;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getWantedNickname(): string
+	{
+		return $this->wantedNickname;
+	}
+
+	/**
+	 * @param string $wantedNickname
+	 */
+	public function setWantedNickname(string $wantedNickname)
+	{
+		$this->wantedNickname = $wantedNickname;
+	}
+}
