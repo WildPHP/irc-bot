eac2f028fdf24833a12888e387fddc95b81f8647
Yoshi2889 <rick.2889@gmail.com>
2017-06-29 00:47:06 +0200

    Re-add ComponentContainer... Oops
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/ComponentContainer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace WildPHP\Core;
+
+use React\EventLoop\LoopInterface;
+
+class ComponentContainer extends \Yoshi2889\Container\ComponentContainer
+{
+	/**
+	 * @var LoopInterface
+	 */
+	protected $loop;
+
+	/**
+	 * @return LoopInterface
+	 */
+	public function getLoop(): LoopInterface
+	{
+		return $this->loop;
+	}
+
+	/**
+	 * @param LoopInterface $loop
+	 */
+	public function setLoop(LoopInterface $loop)
+	{
+		$this->loop = $loop;
+	}
+}
