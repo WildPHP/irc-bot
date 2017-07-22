2ab11be4600804c5911db1390629896f6d6299b7
Yoshi2889 <rick.2889@gmail.com>
2017-07-22 15:51:53 +0200

    Define WHOIS and WHOWAS
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Connection/IRCMessages/WHOIS.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: rick2
+ * Date: 22-7-2017
+ * Time: 15:40
+ */
+
+namespace WildPHP\Core\Connection\IRCMessages;
+
+
+/**
+ * Class WHOIS
+ * @package WildPHP\Core\Connection\IRCMessages
+ *
+ * Syntax: WHOIS ([server]) [nickname] ([nickname] ...)
+ */
+class WHOIS extends BaseIRCMessage implements SendableMessage
+{
+	protected static $verb = 'WHOIS';
+
+	/**
+	 * @var string
+	 */
+	protected $server = '';
+
+	/**
+	 * @var string[]
+	 */
+	protected $nicknames = '';
+
+	public function __construct($nicknames, string $server = '')
+	{
+		if (is_string($nicknames))
+			$nicknames = [$nicknames];
+
+		$this->setNicknames($nicknames);
+		$this->setServer($server);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getServer(): string
+	{
+		return $this->server;
+	}
+
+	/**
+	 * @param string $server
+	 */
+	public function setServer(string $server)
+	{
+		$this->server = $server;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getNicknames()
+	{
+		return $this->nicknames;
+	}
+
+	/**
+	 * @param string[] $nicknames
+	 */
+	public function setNicknames($nicknames)
+	{
+		$this->nicknames = $nicknames;
+	}
+
+	public function __toString()
+	{
+		$server = !empty($this->getServer()) ? $this->getServer() . ' ' : '';
+		return 'WHOIS ' . $server . implode(',', $this->getNicknames());
+	}
+}
---
+++ src/Connection/IRCMessages/WHOWAS.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: rick2
+ * Date: 22-7-2017
+ * Time: 15:40
+ */
+
+namespace WildPHP\Core\Connection\IRCMessages;
+
+
+/**
+ * Class WHOWAS
+ * @package WildPHP\Core\Connection\IRCMessages
+ *
+ * Syntax: WHOIS [nickname](,[nickname],...) ([count] ([server]))
+ */
+class WHOWAS extends BaseIRCMessage implements SendableMessage
+{
+	protected static $verb = 'WHOIS';
+
+	/**
+	 * @var string
+	 */
+	protected $server = '';
+
+	/**
+	 * @var int
+	 */
+	protected $count = 0;
+
+	/**
+	 * @var string[]
+	 */
+	protected $nicknames = '';
+
+	public function __construct($nicknames, int $count = 0, string $server = '')
+	{
+		if (is_string($nicknames))
+			$nicknames = [$nicknames];
+
+		$this->setNicknames($nicknames);
+		$this->setCount($count);
+		$this->setServer($server);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getServer(): string
+	{
+		return $this->server;
+	}
+
+	/**
+	 * @param string $server
+	 */
+	public function setServer(string $server)
+	{
+		$this->server = $server;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getNicknames()
+	{
+		return $this->nicknames;
+	}
+
+	/**
+	 * @param string[] $nicknames
+	 */
+	public function setNicknames($nicknames)
+	{
+		$this->nicknames = $nicknames;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getCount(): int
+	{
+		return $this->count;
+	}
+
+	/**
+	 * @param int $count
+	 */
+	public function setCount(int $count)
+	{
+		$this->count = $count;
+	}
+
+	public function __toString()
+	{
+		$count = !empty($this->getCount()) ? ' ' . trim($this->getCount() . ' ' . $this->getServer()) : '';
+		return 'WHOWAS ' .  implode(',', $this->getNicknames()) . $count;
+	}
+}
