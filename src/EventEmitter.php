cef08c0a8a9b3e8d65762d854988972d61953c48
Yoshi2889 <rick.2889@gmail.com>
2017-07-22 15:29:21 +0200

    Add first method to EventEmitter
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/EventEmitter.php
+++ src/EventEmitter.php
@@ -14,4 +14,23 @@ use Yoshi2889\Container\ComponentTrait;
 class EventEmitter extends \Evenement\EventEmitter implements ComponentInterface
 {
 	use ComponentTrait;
+
+	/**
+	 * Puts a listener first in the listener array.
+	 *
+	 * @param string $event
+	 * @param callable $listener
+	 *
+	 * @return $this
+	 */
+	public function first(string $event, callable $listener)
+	{
+		if (!isset($this->listeners[$event])) {
+			$this->listeners[$event] = [];
+		}
+
+		array_unshift($this->listeners[$event], $listener);
+
+		return $this;
+	}
 }
