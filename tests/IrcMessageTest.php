ef53e81805a745cdbf766a8d4119e340b9ccd72d
Yoshi2889 <rick.2889@gmail.com>
2017-07-25 18:00:14 +0200

    Test passing an invalid IncomingIrcMessage to message defs.
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- tests/IrcMessageTest.php
+++ tests/IrcMessageTest.php
@@ -60,6 +60,12 @@ class IrcMessageTest extends TestCase
 		$account = new ACCOUNT('ircAccount');
 
 		static::assertEquals('ircAccount', $account->getAccountName());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		ACCOUNT::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testAccountReceive()
@@ -90,6 +96,12 @@ class IrcMessageTest extends TestCase
 		$authenticate = AUTHENTICATE::fromIncomingIrcMessage($incoming);
 
 		static::assertEquals('+', $authenticate->getResponse());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		AUTHENTICATE::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testAwayCreate()
@@ -110,6 +122,12 @@ class IrcMessageTest extends TestCase
 
 		static::assertEquals('nickname', $away->getNickname());
 		static::assertEquals('A sample message', $away->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		AWAY::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testCapCreate()
@@ -132,6 +150,12 @@ class IrcMessageTest extends TestCase
         static::assertEquals('LS', $cap->getCommand());
         static::assertEquals(['cap1', 'cap2'], $cap->getCapabilities());
         static::assertEquals('*', $cap->getNickname());
+
+	    $message = ':server TEEHEE argument' . "\r\n";
+	    $parsedLine = Parser::parseLine($message);
+	    $incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+	    $this->expectException(\InvalidArgumentException::class);
+	    CAP::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testErrorReceive()
@@ -141,6 +165,12 @@ class IrcMessageTest extends TestCase
 		$error = ERROR::fromIncomingIrcMessage($incoming);
 
 		static::assertEquals('A sample message', $error->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		ERROR::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testJoinCreate()
@@ -183,6 +213,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals(['#channel'], $join->getChannels());
 		static::assertEquals('', $join->getIrcAccount());
 		static::assertEquals('', $join->getRealname());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		JOIN::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testKickCreate()
@@ -209,6 +245,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('othernickname', $kick->getTarget());
 		static::assertEquals('#somechannel', $kick->getChannel());
 		static::assertEquals('You deserved it!', $kick->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		KICK::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testModeCreate()
@@ -235,6 +277,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $mode->getNickname());
 		static::assertEquals('-o+b', $mode->getFlags());
 		static::assertEquals(['arg1', 'arg2'], $mode->getArguments());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		MODE::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testModeReceiveUser()
@@ -294,6 +342,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals($userPrefix, $nick->getPrefix());
 		static::assertEquals('nickname', $nick->getNickname());
 		static::assertEquals('newnickname', $nick->getNewNickname());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		NICK::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testNoticeCreate()
@@ -317,6 +371,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals($userPrefix, $notice->getPrefix());
 		static::assertEquals('#somechannel', $notice->getChannel());
 		static::assertEquals('This is a test message', $notice->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		NOTICE::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testPartCreate()
@@ -341,6 +401,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $part->getNickname());
 		static::assertEquals(['#channel'], $part->getChannels());
 		static::assertEquals('I have a valid reason', $part->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		PART::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testPassCreate()
@@ -372,6 +438,12 @@ class IrcMessageTest extends TestCase
 
 		static::assertEquals('testserver1', $ping->getServer1());
 		static::assertEquals('testserver2', $ping->getServer2());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		PING::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testPongCreate()
@@ -393,6 +465,12 @@ class IrcMessageTest extends TestCase
 
 		static::assertEquals('testserver1', $pong->getServer1());
 		static::assertEquals('testserver2', $pong->getServer2());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		PONG::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testPrivmsgCreate()
@@ -416,6 +494,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals($userPrefix, $privmsg->getPrefix());
 		static::assertEquals('#somechannel', $privmsg->getChannel());
 		static::assertEquals('This is a test message', $privmsg->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		PRIVMSG::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testQuitCreate()
@@ -438,6 +522,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals($userPrefix, $quit->getPrefix());
 		static::assertEquals('nickname', $quit->getNickname());
 		static::assertEquals('A sample message', $quit->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		QUIT::fromIncomingIrcMessage($incomingIrcMessage);
 	}
 
 	public function testRawCreate()
@@ -459,6 +549,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $rpl_endofnames->getNickname());
 		static::assertEquals('#channel', $rpl_endofnames->getChannel());
 		static::assertEquals('End of /NAMES list.', $rpl_endofnames->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_ENDOFNAMES::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testRplIsupportReceive()
@@ -471,6 +567,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('server', $rpl_isupport->getServer());
 		static::assertEquals('nickname', $rpl_isupport->getNickname());
 		static::assertEquals('are supported by this server', $rpl_isupport->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_ISUPPORT::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testRplNamReplyReceive()
@@ -483,6 +585,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $rpl_namreply->getNickname());
 		static::assertEquals('+', $rpl_namreply->getVisibility());
 		static::assertEquals(['nickname1', 'nickname2', 'nickname3'], $rpl_namreply->getNicknames());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_NAMREPLY::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testRplTopicReceive()
@@ -495,6 +603,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $rpl_topic->getNickname());
 		static::assertEquals('#channel', $rpl_topic->getChannel());
 		static::assertEquals('A new topic message', $rpl_topic->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_TOPIC::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testRplWelcomeReceive()
@@ -506,6 +620,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('server', $rpl_welcome->getServer());
 		static::assertEquals('nickname', $rpl_welcome->getNickname());
 		static::assertEquals('Welcome to server!', $rpl_welcome->getMessage());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_WELCOME::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testRplWhosPCRplReceive()
@@ -521,6 +641,12 @@ class IrcMessageTest extends TestCase
 		static::assertEquals('nickname', $rpl_whospcrpl->getNickname());
 		static::assertEquals('status', $rpl_whospcrpl->getStatus());
 		static::assertEquals('accountname', $rpl_whospcrpl->getAccountname());
+
+		$message = ':server TEEHEE argument' . "\r\n";
+		$parsedLine = Parser::parseLine($message);
+		$incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+		$this->expectException(\InvalidArgumentException::class);
+		RPL_WHOSPCRPL::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testTopicCreate()
@@ -544,6 +670,12 @@ class IrcMessageTest extends TestCase
 	    static::assertEquals($userPrefix, $topic->getPrefix());
         static::assertEquals('#someChannel', $topic->getChannel());
         static::assertEquals('This is a new topic', $topic->getMessage());
+
+	    $message = ':server TEEHEE argument' . "\r\n";
+	    $parsedLine = Parser::parseLine($message);
+	    $incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+	    $this->expectException(\InvalidArgumentException::class);
+	    TOPIC::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
     public function testUserCreate()
@@ -569,6 +701,12 @@ class IrcMessageTest extends TestCase
         static::assertEquals('localhost', $user->getHostname());
         static::assertEquals('someserver', $user->getServername());
         static::assertEquals('arealname', $user->getRealname());
+
+	    $message = ':server TEEHEE argument' . "\r\n";
+	    $parsedLine = Parser::parseLine($message);
+	    $incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+	    $this->expectException(\InvalidArgumentException::class);
+	    USER::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testVersionCreate()
@@ -605,6 +743,12 @@ class IrcMessageTest extends TestCase
 	    static::assertEquals($userPrefix, $who->getPrefix());
         static::assertEquals('#someChannel', $who->getChannel());
         static::assertEquals('%nuhaf', $who->getOptions());
+
+	    $message = ':server TEEHEE argument' . "\r\n";
+	    $parsedLine = Parser::parseLine($message);
+	    $incomingIrcMessage = new IncomingIrcMessage($parsedLine);
+	    $this->expectException(\InvalidArgumentException::class);
+	    WHO::fromIncomingIrcMessage($incomingIrcMessage);
     }
 
 	public function testWhoisCreate()
