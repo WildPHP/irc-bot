ff63a4d7889d1478865b4a2aeff83f5e66d45b11
Yoshi2889 <rick.2889@gmail.com>
2017-07-25 16:05:40 +0200

    Add tests for UserPrefix
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ tests/UserPrefixTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+use PHPUnit\Framework\TestCase;
+use WildPHP\Core\Connection\UserPrefix;
+
+class UserPrefixTest extends TestCase
+{
+	public function testRegularPrefix()
+	{
+		$prefix = 'nickname!username@hostname';
+		$userPrefixObject = UserPrefix::fromString($prefix);
+
+		$expected = new UserPrefix('nickname', 'username', 'hostname');
+		self::assertEquals($expected, $userPrefixObject);
+
+		$prefix = 'nickname!username';
+		$userPrefixObject = UserPrefix::fromString($prefix);
+
+		$expected = new UserPrefix('nickname', 'username');
+		self::assertEquals($expected, $userPrefixObject);
+
+		$prefix = 'nickname@hostname';
+		$userPrefixObject = UserPrefix::fromString($prefix);
+
+		$expected = new UserPrefix('nickname', '', 'hostname');
+		self::assertEquals($expected, $userPrefixObject);
+	}
+
+	public function testOnlyServerPrefix()
+	{
+		$prefix = 'irc.example.com';
+		$userPrefixObject = UserPrefix::fromString($prefix);
+
+		$expected = new UserPrefix('', '', 'irc.example.com');
+		self::assertEquals($expected, $userPrefixObject);
+	}
+}
