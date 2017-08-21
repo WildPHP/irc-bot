5805c449dc7e40fc333177af77278d608ebcbc89
Yoshi2889 <rick.2889@gmail.com>
2017-08-21 21:02:18 +0200

    Add PermissionGroupCollectionTest
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ tests/PermissionGroupCollectionTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+use PHPUnit\Framework\TestCase;
+use WildPHP\Core\Permissions\PermissionGroup;
+use WildPHP\Core\Permissions\PermissionGroupCollection;
+
+class PermissionGroupCollectionTest extends TestCase
+{
+	public function setUp()
+	{
+		if (!defined('WPHP_ROOT_DIR'))
+			define('WPHP_ROOT_DIR', dirname(__FILE__));
+	}
+	
+	public function testGetStoredGroupData()
+	{
+		$permissionGroupCollection = new PermissionGroupCollection();
+		
+		$expected = [
+			'modeGroup' => 0,
+			'userCollection' => [
+				'ircAccount'
+			],
+			'allowedPermissions' => [
+				'testing'
+			],
+			'channelCollection' => []
+		];
+		
+		self::assertEquals($expected, $permissionGroupCollection->getStoredGroupData('testGroup'));
+	}
+
+	public function testFindGroupsForIrcAccount()
+	{
+		$ircAccount = 'ircAccount';
+
+		$permissionGroupCollection = new PermissionGroupCollection();
+		$groupState = $permissionGroupCollection->getStoredGroupData('testGroup');
+		
+		$expectedGroup = new PermissionGroup($groupState);
+		$permissionGroupCollection->offsetSet(0, $expectedGroup);
+		
+		$groups = $permissionGroupCollection->findAllGroupsForIrcAccount($ircAccount);
+		
+		self::assertEquals($expectedGroup, $groups[0]);
+	}
+}
