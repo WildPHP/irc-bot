29934b9f610c1056d60d58ba9302a65fc4f212cc
Yoshi2889 <rick.2889@gmail.com>
2017-08-21 21:10:36 +0200

    RAW cannot be received
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/Connection/IRCMessages/RAW.php
+++ src/Connection/IRCMessages/RAW.php
@@ -8,15 +8,13 @@
 
 namespace WildPHP\Core\Connection\IRCMessages;
 
-use WildPHP\Core\Connection\IncomingIrcMessage;
-
 /**
  * Class RAW
  * @package WildPHP\Core\Connection\IRCMessages
  *
  * Syntax: prefix RAW nickname/channel options
  */
-class RAW extends BaseIRCMessage implements ReceivableMessage, SendableMessage
+class RAW extends BaseIRCMessage implements SendableMessage
 {
 	/**
 	 * @var string
@@ -31,17 +29,6 @@ class RAW extends BaseIRCMessage implements ReceivableMessage, SendableMessage
 	public function __construct(string $command)
 	{
 		$this->setCommand($command);
-	}
-
-	/**
-	 * @param IncomingIrcMessage $incomingIrcMessage
-	 *
-	 * @return \self
-	 * @throws \ErrorException
-	 */
-	public static function fromIncomingIrcMessage(IncomingIrcMessage $incomingIrcMessage): self
-	{
-		throw new \ErrorException('You could not have received this message... :o');
 	}
 
 	/**
