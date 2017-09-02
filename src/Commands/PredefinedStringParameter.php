ef0674774ddbf8c44d08357726b700d75bde0ef6
Yoshi2889 <rick.2889@gmail.com>
2017-09-02 14:31:28 +0200

    Add PredefinedStringParameter
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

---
+++ src/Commands/PredefinedStringParameter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright 2017 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace WildPHP\Core\Commands;
+
+class PredefinedStringParameter extends Parameter
+{
+	/**
+	 * PredefinedStringParameter constructor.
+	 *
+	 * @param string $expected
+	 */
+	public function __construct(string $expected)
+	{
+		parent::__construct(function (string $value) use ($expected)
+		{
+			return $value == $expected;
+		});
+	}
+}
