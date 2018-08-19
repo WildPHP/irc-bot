3c13d1415533d0f935d10ae156ee7aae9916d891
NanoSector <rick@nanosector.nl>
2018-08-19 13:14:18 +0200

    Update copyright

--- src/Connection/IRCMessages/SendableMessage.php
+++ src/Connection/IRCMessages/SendableMessage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2017 The WildPHP Team
+ * Copyright 2018 The WildPHP Team
  *
  * You should have received a copy of the MIT license with the project.
  * See the LICENSE file for more information.
