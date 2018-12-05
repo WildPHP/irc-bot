73a6a93b1a9ea4c2552f76460c4023c190196c7d
Rick Kerkhof <rick.2889@gmail.com>
2018-12-05 16:09:49 +0100

    Add UserModeParser
    Signed-off-by: Rick Kerkhof <rick.2889@gmail.com>

---
+++ src/Connection/UserModeParser.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2018 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace WildPHP\Core\Connection;
+
+
+class UserModeParser
+{
+    /**
+     * @var array
+     */
+    protected static $prefixMap = [
+        '@' => 'o',
+        '%' => 'h',
+        '+' => 'v'
+    ];
+
+    /**
+     * @param string $nickname
+     * @param string $remainders
+     *
+     * @return array
+     */
+    public static function extractFromNickname(string $nickname, string &$remainders): array
+    {
+        $parts = str_split($nickname);
+        $modes = [];
+
+        foreach ($parts as $key => $part) {
+            if (!array_key_exists($part, self::$prefixMap)) {
+                $remainders = join('', $parts);
+                break;
+            }
+
+            unset($parts[$key]);
+            $modes[] = self::$prefixMap[$part];
+        }
+
+        return $modes;
+    }
+}
