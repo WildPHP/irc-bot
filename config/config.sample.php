7df419cb401d203449f01812a3e10bc6307e2b4f
Rick Kerkhof <rick.2889@gmail.com>
2018-12-11 20:21:28 +0100

    Rewrite configuration file to use PHP instead of NEON
    Signed-off-by: Rick Kerkhof <rick.2889@gmail.com>

---
+++ config/config.sample.php
@@ -0,0 +1,87 @@
+<?php
+
+use WildPHP\Core\Storage\Providers\SQLiteDatabaseStorageProvider;
+
+return [
+    'connection' => [
+        # The server to connect to.
+        # The secure option turns on SSL for this connection.
+        # Make sure to use an SSL-enabled server and port before turning this on.
+        'server' => 'irc.freenode.net',
+        'port' => 6697,
+        'secure' => true,
+
+        # Nickname to use.
+        # Takes an array of strings to swap between nicknames if the first one is not available.
+        'nickname' => [
+            'MyBot'
+        ],
+
+        # The username to use. Not really used by modern IRC servers, pick a short string.
+        'username' => 'MyBot',
+
+        # The realname to use, identifying the bot on the network. May be a string of your choosing.
+        'realname' => 'A WildPHP Bot',
+
+        # Optional password to be sent while connecting to the server. Leave blank to disable.
+        'password' => '',
+
+        # The channels to join after connecting.
+        'channels' => [
+            '#channel'
+        ],
+
+        # Uncomment the following to enable SASL functionality if your network supports it.
+        # This can be used to for example identify with services while connecting,
+        # instead of logging in to services afterwards.
+        //'sasl' => [
+        //    'username' => 'myUsername',
+        //    'password' => 'mySekritPassword'
+        //],
+
+        # You can set additional connection options here.
+        # Please see https://github.com/reactphp/socket#connector for more details.
+        # Example options below:
+        //'options'  => [
+        //    'dns' => false,
+        //    'tcp' => [
+        //        'bindto' => '192.168.1.11'
+        //    ]
+    ],
+
+    # Storage driver used for persistent storage
+    # SQLite is used by default, but for bots in large networks it might be faster to use the MySQL storage provider.
+    # Available configuration options depend on the chosen provider.
+    #
+    # Available providers:
+    # - SQLite (default): \WildPHP\Core\Storage\Providers\SQLiteDatabaseStorageProvider
+    # - MySQL: \WildPHP\Core\Storage\Providers\MySQLDatabaseStorageProvider
+    # - FlatFile (NOT RECOMMENDED): \WildPHP\Core\Storage\Providers\FlatFileStorageProvider
+    'storage' => [
+        'driver' => new SQLiteDatabaseStorageProvider('../storage/persistent.sqlite')
+    ],
+
+    # Command prefix used for command parsing.
+    'prefix' => '!',
+
+    # The bot owner who gets all permissions by default.
+    # Set this to the username used to identify with authentication services like NickServ (freenode)
+    'owner' => 'NickServUsername',
+
+    # The log level to use.
+    # One of debug, info, warning, error
+    # It is recommended to leave this in debug for now as the other modes barely show information.
+    'log_level' => 'debug',
+
+    # The list of modules to use. Each line takes a PHP class name;
+    # consult the module documentation if you are unsure what to place here.
+    # Please note some modules may require additional configuration, please put this configuration below this section.
+    'modules' => [
+
+    ],
+
+    ###
+    ### MODULE SPECIFIC CONFIGURATION BELOW
+    ###
+
+];
