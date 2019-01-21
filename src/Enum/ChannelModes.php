7199dace7496b5fbea234bb34a746e9a54f2d0a5
Rick Kerkhof <rick.2889@gmail.com>
2019-01-21 14:29:08 +0100

    Public static const makes no sense...
    Also set Scrutinizer to use PHP 7.1
    
    Signed-off-by: Rick Kerkhof <rick.2889@gmail.com>

--- src/Enum/ChannelModes.php
+++ src/Enum/ChannelModes.php
@@ -10,48 +10,48 @@ namespace WildPHP\Core\Enum;
 
 class ChannelModes extends Enum
 {
-    public static const PREFIX_OWNER = '~';
-    public static const PREFIX_ADMIN = '&';
-    public static const PREFIX_OP = '@';
-    public static const PREFIX_HALFOP = '%';
-    public static const PREFIX_VOICE = '+';
+    public const PREFIX_OWNER = '~';
+    public const PREFIX_ADMIN = '&';
+    public const PREFIX_OP = '@';
+    public const PREFIX_HALFOP = '%';
+    public const PREFIX_VOICE = '+';
 
-    public static const ACCESS_OWNER = 'h';
-    public static const ACCESS_ADMIN = 'a';
-    public static const ACCESS_OP = 'o';
-    public static const ACCESS_VOICE = 'v';
+    public const ACCESS_OWNER = 'h';
+    public const ACCESS_ADMIN = 'a';
+    public const ACCESS_OP = 'o';
+    public const ACCESS_VOICE = 'v';
 
-    public static const LIST_BAN = 'b';
-    public static const LIST_BAN_EXCEPT = 'e';
-    public static const LIST_INVITE_EXCEPT = 'l';
+    public const LIST_BAN = 'b';
+    public const LIST_BAN_EXCEPT = 'e';
+    public const LIST_INVITE_EXCEPT = 'l';
 
-    public static const SETTING_NO_COLOR = 'c';
-    public static const SETTING_NO_CTCP = 'C';
-    public static const SETTING_DELAY_JOIN = 'D';
-    public static const SETTING_DELAY_JOIN_UNSET_TEMP = 'd';
-    public static const SETTING_FLOOD_PROTECTION = 'f';
-    public static const SETTING_CENSOR = 'G';
-    public static const SETTING_INVITE_ONLY = 'i';
-    public static const SETTING_CHANNEL_KEY = 'k';
-    public static const SETTING_NO_KNOCK = 'K';
-    public static const SETTING_CHANNEL_LINK = 'L';
-    public static const SETTING_USER_LIMIT = 'l';
-    public static const SETTING_MODERATED = 'm';
-    public static const SETTING_REGISTERED_ONLY_SPEAK = 'M';
-    public static const SETTING_NO_NICK_CHANGE = 'N';
-    public static const SETTING_NO_EXTERNAL_MESSAGES = 'n';
-    public static const SETTING_IRCOP_ONLY = 'O';
-    public static const SETTING_PERMANENT = 'P';
-    public static const SETTING_PRIVATE = 'p';
-    public static const SETTING_NO_KICK = 'Q';
-    public static const SETTING_REGISTERED_ONLY = 'R';
-    public static const SETTING_CHANNEL_REGISTERED = 'r';
-    public static const SETTING_SECRET = 's';
-    public static const SETTING_STRIP_COLOR = 'S';
-    public static const SETTING_NO_NOTICE = 'T';
-    public static const SETTING_RESTRICT_TOPIC = 't';
-    public static const SETTING_NO_INVITE = 'V';
-    public static const SETTING_SECURE_ONLY = 'z';
-    public static const SETTING_IS_SECURE = 'Z';
+    public const SETTING_NO_COLOR = 'c';
+    public const SETTING_NO_CTCP = 'C';
+    public const SETTING_DELAY_JOIN = 'D';
+    public const SETTING_DELAY_JOIN_UNSET_TEMP = 'd';
+    public const SETTING_FLOOD_PROTECTION = 'f';
+    public const SETTING_CENSOR = 'G';
+    public const SETTING_INVITE_ONLY = 'i';
+    public const SETTING_CHANNEL_KEY = 'k';
+    public const SETTING_NO_KNOCK = 'K';
+    public const SETTING_CHANNEL_LINK = 'L';
+    public const SETTING_USER_LIMIT = 'l';
+    public const SETTING_MODERATED = 'm';
+    public const SETTING_REGISTERED_ONLY_SPEAK = 'M';
+    public const SETTING_NO_NICK_CHANGE = 'N';
+    public const SETTING_NO_EXTERNAL_MESSAGES = 'n';
+    public const SETTING_IRCOP_ONLY = 'O';
+    public const SETTING_PERMANENT = 'P';
+    public const SETTING_PRIVATE = 'p';
+    public const SETTING_NO_KICK = 'Q';
+    public const SETTING_REGISTERED_ONLY = 'R';
+    public const SETTING_CHANNEL_REGISTERED = 'r';
+    public const SETTING_SECRET = 's';
+    public const SETTING_STRIP_COLOR = 'S';
+    public const SETTING_NO_NOTICE = 'T';
+    public const SETTING_RESTRICT_TOPIC = 't';
+    public const SETTING_NO_INVITE = 'V';
+    public const SETTING_SECURE_ONLY = 'z';
+    public const SETTING_IS_SECURE = 'Z';
 
 }
--- src/Enum/UserModes.php
+++ src/Enum/UserModes.php
@@ -11,25 +11,25 @@ namespace WildPHP\Core\Enum;
 
 class UserModes extends Enum
 {
-    public static const IS_BOT = 'B';
-    public static const ONLY_PREFIXED_MESSAGES = 'd';
-    public static const NO_PRIVATE_MESSAGES = 'D';
-    public static const CENSOR = 'G';
-    public static const HIDE_IRCOP = 'H';
-    public static const HIDE_ONLINE = 'I';
-    public static const INVISIBLE = 'i';
-    public static const IRCOP = 'o';
-    public static const HIDE_JOINED_CHANNELS = 'p';
-    public static const NO_KICK = 'q';
-    public static const REGISTERED_NICKNAME = 'r';
-    public static const ONLY_REGISTERED_PRIVATE_MESSAGES = 'R';
-    public static const SERVICES_BOT = 'S';
-    public static const SERVER_NOTICE_MASKS = 's';
-    public static const NO_CTCP = 'T';
-    public static const USING_VHOST = 't';
-    public static const SHOW_WHOIS = 'W';
-    public static const LISTEN_WALLOPS = 'w';
-    public static const HIDDEN_NICKNAME = 'x';
-    public static const ONLY_SECURE_PRIVATE_MESSAGES = 'Z';
-    public static const USING_SECURE_CONNECTION = 'z';
+    public const IS_BOT = 'B';
+    public const ONLY_PREFIXED_MESSAGES = 'd';
+    public const NO_PRIVATE_MESSAGES = 'D';
+    public const CENSOR = 'G';
+    public const HIDE_IRCOP = 'H';
+    public const HIDE_ONLINE = 'I';
+    public const INVISIBLE = 'i';
+    public const IRCOP = 'o';
+    public const HIDE_JOINED_CHANNELS = 'p';
+    public const NO_KICK = 'q';
+    public const REGISTERED_NICKNAME = 'r';
+    public const ONLY_REGISTERED_PRIVATE_MESSAGES = 'R';
+    public const SERVICES_BOT = 'S';
+    public const SERVER_NOTICE_MASKS = 's';
+    public const NO_CTCP = 'T';
+    public const USING_VHOST = 't';
+    public const SHOW_WHOIS = 'W';
+    public const LISTEN_WALLOPS = 'w';
+    public const HIDDEN_NICKNAME = 'x';
+    public const ONLY_SECURE_PRIVATE_MESSAGES = 'Z';
+    public const USING_SECURE_CONNECTION = 'z';
 }
