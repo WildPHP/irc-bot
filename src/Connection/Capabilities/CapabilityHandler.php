8e727ceba486377ed5b08d8f191397c38b5d7cc2
Yoshi2889 <rick@nanosector.nl>
2019-04-15 21:04:40 +0200

    Remove duplicate copyright, again...
    Signed-off-by: Yoshi2889 <rick@nanosector.nl>

--- src/Connection/Capabilities/CapabilityHandler.php
+++ src/Connection/Capabilities/CapabilityHandler.php
@@ -8,13 +8,6 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright 2019 The WildPHP Team
- *
- * You should have received a copy of the MIT license with the project.
- * See the LICENSE file for more information.
- */
-
 namespace WildPHP\Core\Connection\Capabilities;
 
 use Evenement\EventEmitterInterface;
