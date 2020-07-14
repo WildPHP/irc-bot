a5ec35ec7a2ce7832724e0035114936c637f4f0d
NanoSector <rick@nanosector.nl>
2020-07-14 16:35:46 +0200

    Add tests for ConnectionDetails::fromArray

--- tests/Connection/ConnectionDetailsTest.php
+++ tests/Connection/ConnectionDetailsTest.php
@@ -82,8 +82,74 @@ class ConnectionDetailsTest extends TestCase
     {
         $options = ['test'];
 
-        $connectionDetails = new ConnectionDetails('Test', 'Test', 'Test', 9999, 'Test', 'Test', 'Test', true,
-            $options);
+        $connectionDetails = new ConnectionDetails(
+            'Test', 'Test', 'Test', 9999, 'Test', 'Test', 'Test', true,
+            $options
+        );
         self::assertEquals($options, $connectionDetails->getContextOptions());
+    }
+
+    public function testFromArray()
+    {
+        $details = [
+            'connection' => [
+                'server' => 'irc.freenode.net',
+                'port' => 6697,
+                'secure' => true,
+                'nickname' => [
+                    'MyBot'
+                ],
+                'username' => 'MyBot',
+                'realname' => 'A WildPHP Bot',
+                'password' => 'tester'
+            ],
+        ];
+
+        $connectionDetails = ConnectionDetails::fromArray($details);
+
+        self::assertEquals('irc.freenode.net', $connectionDetails->getAddress());
+        self::assertEquals(6697, $connectionDetails->getPort());
+        self::assertTrue($connectionDetails->getSecure());
+        self::assertEquals('MyBot', $connectionDetails->getWantedNickname());
+        self::assertEquals('MyBot', $connectionDetails->getUsername());
+        self::assertEquals('A WildPHP Bot', $connectionDetails->getRealname());
+        self::assertEquals('tester', $connectionDetails->getPassword());
+    }
+
+    public function testFromArrayMissingRootKey()
+    {
+        $details = [
+            'server' => 'irc.freenode.net',
+            'port' => 6697,
+            'secure' => true,
+            'nickname' => [
+                'MyBot'
+            ],
+            'username' => 'MyBot',
+            'realname' => 'A WildPHP Bot',
+            'password' => 'tester'
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+        ConnectionDetails::fromArray($details);
+    }
+
+    public function testFromArrayMissingMandatoryKeys()
+    {
+        $details = [
+            'connection' => [
+                // server and port are missing
+                'secure' => true,
+                'nickname' => [
+                    'MyBot'
+                ],
+                'username' => 'MyBot',
+                'realname' => 'A WildPHP Bot',
+                'password' => 'tester'
+            ],
+        ];
+
+        $this->expectException(\InvalidArgumentException::class);
+        ConnectionDetails::fromArray($details);
     }
 }
