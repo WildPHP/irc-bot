333ec80acde9e7e5ad76465462ba9cadff4f53e7
NanoSector <rick@nanosector.nl>
2020-12-28 17:56:29 +0100

    Remove unused dependencies on ext-pdo and ext-posix

--- bin/wildphp.php
+++ bin/wildphp.php
@@ -12,10 +12,11 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 
 if (PHP_SAPI !== 'cli') {
-    echo 'WildPHP must be run from the terminal!';
+    echo 'WildPHP must be run from the terminal!' . PHP_EOL;
     exit(127);
 }
 
+/** @noinspection PhpComposerExtensionStubsInspection */
 if (function_exists('posix_getuid') && posix_getuid() === 0) {
     echo 'Running wildphp as root is not allowed.' . PHP_EOL;
     exit(128);
